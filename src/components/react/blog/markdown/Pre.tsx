864947166ab366244f7be5ea763d733d90959707
SasquatchPunter <dasfunc@gmail.com>
2024-05-16 12:02:10 -0600

    feat: add component utils for joining class strings and style strings & objects

--- src/components/react/blog/markdown/Pre.tsx
+++ src/components/react/blog/markdown/Pre.tsx
@@ -1,9 +1,13 @@
+import * as classes from "@utils/components/classes";
+import * as styles from "@utils/components/styles";
+
 export default function Pre(props) {
   const className = "p-2 rounded-xl";
+  const style = "";
   return (
     <pre
-      className={[props.className, className].join(" ")}
-      style={props.style}
+      className={classes.join(props.className, className)}
+      style={styles.join(props.style, style)}
       tabIndex={props.tabIndex}
     >
       {props.children}
