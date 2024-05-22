7a900843a2ba46f0f5bde174d127e4d275ee76a9
SasquatchPunter <dasfunc@gmail.com>
2024-05-22 17:04:01 -0600

    style: remove empty line

--- src/components/react/blog/markdown/code_block.tsx
+++ src/components/react/blog/markdown/code_block.tsx
@@ -1,11 +1,9 @@
 import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
 
-/**  */
 export default function Codeblock(props) {
   const className = "";
   const style = "";
-
   return (
     <pre
       className={classes.join(props.className, className)}
