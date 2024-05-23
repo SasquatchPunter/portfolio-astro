16693d7d9408c6140e13265c4e1128a3b866c24e
SasquatchPunter <dasfunc@gmail.com>
2024-05-22 22:45:01 -0600

    feat: styling for post header

--- src/components/react/blog/ExperimentalPostHeader.tsx
+++ src/components/react/blog/ExperimentalPostHeader.tsx
@@ -5,7 +5,7 @@ export default function ExperimentalPostHeader(props) {
   const { data } = useTina<BlogQuery>(props.post);
   return (
     <header className="bg-black text-white">
-      <h1>{data.blog.title}</h1>
+      <h1 className="text-4xl">{data.blog.title}</h1>
     </header>
   );
 }
