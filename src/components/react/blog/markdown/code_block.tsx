ec26e0fb0c9a27e3aaf9bfd0b3305426d9d131de
SasquatchPunter <dasfunc@gmail.com>
2024-05-25 00:50:01 -0600

    feat: add syntax highlighting to code blocks with react-syntax-highlighter

--- src/components/react/blog/markdown/code_block.tsx
+++ src/components/react/blog/markdown/code_block.tsx
@@ -1,15 +1,46 @@
 import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {} from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
-export default function Codeblock(props) {
-  const className = "";
+function PreTag(props) {
+  const className = "rounded-xl";
   const style = "";
   return (
     <pre
       className={classes.join(props.className, className)}
       style={styles.join(props.style, style)}
     >
-      {props.value}
+      {props.children}
     </pre>
+  );
+}
+
+function CodeTag(props) {
+  const className = "";
+  const style = "";
+  return (
+    <code
+      className={classes.join(props.className, className)}
+      style={styles.join(props.style, style)}
+    >
+      {props.children}
+    </code>
+  );
+}
+
+export default function Codeblock(props) {
+  const className = "";
+  const style = "";
+  return (
+    <SyntaxHighlighter
+      children={props.value || ""}
+      language={props.lang || "jsx"}
+      style={atomOneDark}
+      showLineNumbers
+      PreTag={PreTag}
+      CodeTag={CodeTag}
+    />
   );
 }
