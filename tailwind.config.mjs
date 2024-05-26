198bd5699fe9c8228822a55bb9ff1f0df8d3c488
SasquatchPunter <dasfunc@gmail.com>
2024-05-26 04:42:14 -0600

    feat: add TW plugin to port HLJS classes in for syntax highlighting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,20 @@
+import plugin from "tailwindcss/plugin";
+import hljsLight from "react-syntax-highlighter/dist/esm/styles/hljs/nord";
+import hljsDark from "react-syntax-highlighter/dist/esm/styles/hljs/pojoaque";
+
+const hljsLightClasses = Object.entries(hljsLight).reduce(
+  (acc, [key, value]) => {
+    acc[`& .${key}`] = value;
+    return acc;
+  },
+  {},
+);
+
+const hljsDarkClasses = Object.entries(hljsDark).reduce((acc, [key, value]) => {
+  acc[`& .${key}`] = value;
+  return acc;
+}, {});
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -13,5 +30,12 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".hljs-codeblock": hljsLightClasses,
+        ".hljs-codeblock-dark": hljsDarkClasses,
+      });
+    }),
+  ],
 };
