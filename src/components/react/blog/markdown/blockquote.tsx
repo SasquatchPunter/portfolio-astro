7999d3a682d0199e2fcee1eba5fd050445264c2e
SasquatchPunter <dasfunc@gmail.com>
2024-05-26 12:36:58 -0600

    feat: styling blockquote for blog markdown

--- src/components/react/blog/markdown/blockquote.tsx
+++ src/components/react/blog/markdown/blockquote.tsx
@@ -2,7 +2,8 @@ import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
 
 export default function Blockquote(props) {
-  const className = "";
+  const className =
+    "mx-4 px-4 py-2 border-l-2 border-gray-900 dark:border-gray-200 bg-gradient-to-l from-transparent to-gray-200 dark:to-gray-800";
   const style = "";
   return (
     <blockquote
