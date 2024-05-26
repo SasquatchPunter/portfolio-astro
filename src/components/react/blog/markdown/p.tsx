76395c545e55060e5428a35dd986e56e7e21659c
SasquatchPunter <dasfunc@gmail.com>
2024-05-25 20:36:21 -0600

    feat: add margin to P markdown component

--- src/components/react/blog/markdown/p.tsx
+++ src/components/react/blog/markdown/p.tsx
@@ -2,7 +2,7 @@ import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
 
 export default function P(props) {
-  const className = "";
+  const className = "my-4";
   const style = "";
   return (
     <p
