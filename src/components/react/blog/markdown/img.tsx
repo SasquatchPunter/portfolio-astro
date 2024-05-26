cf5b8d9772ea96c22b80de8c0e44045ef6631d28
SasquatchPunter <dasfunc@gmail.com>
2024-05-25 20:35:35 -0600

    fix(img.tsx): pass correct props to *src* and *alt* attributes
    Fix attribute props and transform src url to include astro's BASE_URL

--- src/components/react/blog/markdown/img.tsx
+++ src/components/react/blog/markdown/img.tsx
@@ -2,15 +2,16 @@ import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
 
 export default function Image(props) {
-  const className = "";
+  const className = "rounded-xl";
   const style = "";
+  // props.props.caption is supported by the default tina ui component
+  // this could be added as a visible caption in the future
   return (
     <img
       className={classes.join(props.className, className)}
       style={styles.join(props.style, style)}
-      src={props.src}
-      title={props.title}
-      alt={props.alt}
+      src={`${import.meta.env.BASE_URL}${props.url}`}
+      alt={props.props?.alt}
     />
   );
 }
